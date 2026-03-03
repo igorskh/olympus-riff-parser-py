390c12b10f670bd87633b039c053f5c79bd66b70
Igor Kim <igor.skh@gmail.com>
2026-03-03 22:47:10 +0100

    add index marks and datetime tests

--- tests/test_read.py
+++ tests/test_read.py
@@ -9,3 +9,9 @@ def test_parse_riff_subsection_olym():
     assert "end_datetime" in result
 
     assert len(result["index_marks"]) == 2
+
+    assert result["index_marks"][0] == 4.365
+    assert result["index_marks"][1] == 9.38
+
+    assert result["start_datetime"] == "2026-03-03 22:37:42"
+    assert result["end_datetime"] == "2026-03-03 22:37:56"
